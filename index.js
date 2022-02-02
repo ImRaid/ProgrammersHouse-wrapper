7f881e4b8be66b1067ef5d8a87dbb697c8a60504
Raid <51675128+ImRaid@users.noreply.github.com>
2022-02-02 10:13:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ class apiwrapper {
   * @param {string} [question] - Запитання
   */
   static async eightball(question) {
-    if(!question) throw new TypeError("Вкажи запитання!")
+    if(!question) throw new TypeError("Вкажи запитання!");
     fetch(`htps://www.programmershouse-api.ga/8ball?питання=${question}`).then(resource => resource.json()).then(body => {
       const input = body.ukraine
       return input
@@ -27,9 +27,20 @@ class apiwrapper {
   * @param {string} [avatar_url] - Аватар користувача
   */
   static async ukraine(avatar_url) {
+    if(!avatar_url) throw new TypeError("Вкажи посилання на аватар!");
     fetch(`https://www.programmershouse-api.ga/ukraine?avatar=${avatar_url}`).then(resource => resource.json()).then(body => {
             const input = body.ukraine
             return input
           })
+  }
+  /**
+  * @param {string} [title] - Заголовок посту.
+  * @param {string} [description] - Опис посту.
+  */
+  static async reddit(title, description) {
+    if(!title) throw new TypeError("Вкажи заголовок посту!");
+    if(!description) throw new TypeError("Вкажи опис посту!");
+    const input = `https://www.programmershouse-api.ga/reddit?title=${title}&description=${description}`
+    return input
   }
 }
